c1adbbfc16021e79f1cbf0e257ca694ef7ff341f
octav20 <octav20>
2023-07-24 17:33:43 -0700

    final

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,13 +1,17 @@
-import getCategory from "@/actions/get-category";
-import getColors from "@/actions/get-colors";
-import getProducts from "@/actions/get-products";
-import getSizes from "@/actions/get-sizes";
-import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
-import Filter from "./components/filter";
-import NoResults from "@/components/ui/no-results";
+import Billboard from "@/components/billboard";
 import ProductCard from "@/components/ui/product-card";
+import NoResults from "@/components/ui/no-results";
+
+import getProducts from "@/actions/get-products";
+import getCategory from "@/actions/get-category";
+import getSizes from "@/actions/get-sizes";
+import getColors from "@/actions/get-colors";
+
+import Filter from "./components/filter";
 import MobileFilters from "./components/mobile-filters";
+
+export const revalidate = 0;
 
 interface CategoryPageProps {
   params: {
@@ -18,6 +22,7 @@ interface CategoryPageProps {
     sizeId: string;
   };
 }
+
 const CategoryPage: React.FC<CategoryPageProps> = async ({
   params,
   searchParams,
@@ -27,10 +32,10 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     colorId: searchParams.colorId,
     sizeId: searchParams.sizeId,
   });
-
   const sizes = await getSizes();
   const colors = await getColors();
   const category = await getCategory(params.categoryId);
+
   return (
     <div className="bg-white">
       <Container>
